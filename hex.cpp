1e8400912f7628a44f6a56c0a29ede9cab7336cc
PotatoJoe111 <dbu99r@protonmail.com>
2024-12-19 05:48:30 +0100

    basic buffer operations

--- hex.cpp
+++ hex.cpp
@@ -5,11 +5,15 @@ using namespace std;
 
 int main(int argc, char *argv[]){
 	if(argc!=2){
-		cout << "The program requires a file path" << endl;
+		cout << "The program takes a file path as input" << endl;
 	}
 	string text;
-	ifstream read_file(argv[1]);
-	while(getline(read_file, text)){
+	ifstream ifs(argv[1],ifstream::binary);
+	/*while(getline(ifs, text)){
 		cout << text;
-	}
+	}*/
+	filebuf* pbuf = ifs.rdbuf();
+	size_t size = pbuf->pubseekoff (0,ifs.end,ifs.in);
+	pbuf->pubseekpos (0,ifs.in);
+	cout << size << endl;
 }
