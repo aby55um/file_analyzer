ac5b749fd21035c7b8c46017a1aacb68d7ec8b91
PotatoJoe111 <dbu99r@protonmail.com>
2024-12-23 05:33:44 +0100

    Added zip format

--- sig.cpp
+++ sig.cpp
@@ -21,11 +21,18 @@ string check_signature(char sig[100]){
 
 int init_signature_data(){
 	int len = 4;
-  int test[4] = {0x25,0x50,0x44,0x46};
+  int pdf[4] = {0x25,0x50,0x44,0x46};
   for(int i=0;i<len;i++){
-  	sig_map["PDF"][i] = test[i];
+  	sig_map["PDF"][i] = pdf[i];
   }
-  sig_map["PDF"][100] = 4;
+  sig_map["PDF"][100] = len;
+  
+  int zip[4] = {0x50,0x4b,0x03,0x04};
+  for(int i=0;i<len;i++){
+  	sig_map["ZIP"][i] = zip[i];
+  }
+  sig_map["ZIP"][100] = len;
+  
   return 0;
 }
 
